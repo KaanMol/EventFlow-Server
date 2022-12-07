33e5094127d618078340fd820233c1252d6e416d
Lucas <29230467+Somfic@users.noreply.github.com>
2022-12-07 17:30:34 +0100

    Updated mods

--- ns_scraper/src/lib.rs
+++ ns_scraper/src/lib.rs
@@ -1,17 +1,3 @@
-use headless_chrome::Browser;
-
-pub async fn scrape(url: String) -> String {
-    let browser = Browser::default().unwrap();
-
-    let tab = browser.wait_for_initial_tab().unwrap();
-
-    tab.navigate_to(url.as_str()).unwrap();
-
-    let elem = tab
-        .wait_for_element(".rio-jp-travel-option-active .rio-jp-travel-time > time")
-        .unwrap()
-        .get_inner_text()
-        .unwrap();
-
-    elem
-}
+pub mod route;
+pub mod route_builder;
+pub mod route_finder;
