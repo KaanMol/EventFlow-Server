57660ea697af5badbb20267b82dd87e65cc7202a
Lucas <29230467+Somfic@users.noreply.github.com>
2022-12-07 12:17:49 +0100

    Added Icals migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
+mod m20220101_000001_create_users;
+mod m20221207_121314_create_icals;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_create_table::Migration)]
+        vec![
+            Box::new(m20220101_000001_create_users::Migration),
+            Box::new(m20221207_121314_create_icals::Migration),
+        ]
     }
 }
