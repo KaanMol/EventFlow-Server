53c80dbbe1c46c9c41c40ab7f3df3bee83a69436
Lucas <29230467+Somfic@users.noreply.github.com>
2022-12-17 15:01:01 +0100

    Implemented Display trait for entities

--- src/entity/calendar_event.rs
+++ src/entity/calendar_event.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -14,11 +16,23 @@ pub struct CalendarEvent {
     original_event: OriginalEvent,
 }
 
+impl fmt::Display for CalendarEvent {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", super::to_json(&self))
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Repeat {
     repeat: String,
     repeat_every: u32,
     repeat_until: String,
+}
+
+impl fmt::Display for Repeat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", super::to_json(&self))
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -29,6 +43,12 @@ pub struct Metadata {
     travel_time_minutes: u32,
 }
 
+impl fmt::Display for Metadata {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", super::to_json(&self))
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct OriginalEvent {
     title: String,
@@ -37,4 +57,10 @@ pub struct OriginalEvent {
     end: chrono::NaiveDateTime,
     all_day: bool,
     location: String,
+}
+
+impl fmt::Display for OriginalEvent {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", super::to_json(&self))
+    }
 }
--- src/entity/user.rs
+++ src/entity/user.rs
@@ -1,9 +1,17 @@
+use core::fmt;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct User {
     pub name: String,
     pub sources: Vec<CalenderEventSource>,
+}
+
+impl fmt::Display for User {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", super::to_json(&self))
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -14,6 +22,12 @@ pub struct CalenderEventSource {
     filter: Vec<CalendarEventSourceFilters>,
 }
 
+impl fmt::Display for CalenderEventSource {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", super::to_json(&self))
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CalenderEventSourceModifier {
     operation: String,
@@ -22,10 +36,22 @@ pub struct CalenderEventSourceModifier {
     new_value: String,
 }
 
+impl fmt::Display for CalenderEventSourceModifier {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", super::to_json(&self))
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CalendarEventSourceFilters {
     field: String,
     operator: String,
     value: String,
     calendar_id: String,
+}
+
+impl fmt::Display for CalendarEventSourceFilters {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", super::to_json(&self))
+    }
 }
