31a3ff13f7dabd453c4af4a2a8ae66f3d6e5d857
Kaan <kaanmol@outlook.com>
2023-01-15 00:44:34 +0100

    Rewritten a part of the get user endpoint to the new response format
    A new response format has been created to make all responses static and better to parse for clients. This format can be found at: https://github.com/KaanMol/EventFlow-Server/wiki/Api

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,12 +1,13 @@
 use crate::{
-    entity,
+    entity::{self, user},
+    handlers::error::ResourceError,
     routes::{DataResponse, ErrorResponse, Response},
     AppState,
 };
 use actix_web::{
     error,
     web::{Data, Json, Path},
-    Error, HttpResponse,
+    Error, HttpResponse, Responder,
 };
 
 #[derive(serde::Deserialize, Clone)]
@@ -32,6 +33,7 @@ pub async fn create(
         .await
         .map_err(|e| error::ErrorBadRequest(ErrorResponse::new("Could not create user", e)))?;
 
+    // let user = state.db.collection::<entity::user::User>("users").g
     Ok(HttpResponse::Created().json(DataResponse::new("Created user", result.inserted_id)))
 }
 
@@ -39,25 +41,44 @@ pub async fn create(
 pub async fn read(state: Data<AppState>, user_id: Path<String>) -> Result<HttpResponse, Error> {
     let id = crate::routes::parse_id(user_id.to_string())?;
 
-    let user = state
-        .db
-        .collection::<entity::user::User>("users")
-        .find_one(
-            mongodb::bson::doc! {
-                "_id": id
-            },
-            None,
-        )
-        .await
-        .map_err(|e| error::ErrorBadRequest(ErrorResponse::new("Could not query users", e)))?
-        .ok_or_else(|| {
-            error::ErrorNotFound(Response::new(format!(
-                "Could not find user with id {}",
-                user_id
-            )))
-        })?;
+    let user = crate::handlers::user::get_user(id, state).await;
+    match user {
+        Ok(user) => Ok(HttpResponse::Ok().json(DataResponse::new("Found user", user))),
+        Err(error) => Ok(error_handler(error)),
+    }
+}
 
-    Ok(HttpResponse::Ok().json(DataResponse::new("Found user", user)))
+#[derive(serde::Serialize)]
+struct ApiResponse<T> {
+    success: bool,
+    error: Option<ResponseError>,
+    data: Option<T>,
+}
+#[derive(serde::Serialize)]
+struct ResponseError {
+    code: i32,
+    message: String,
+}
+
+pub fn error_handler(error: crate::handlers::error::ResourceError) -> HttpResponse {
+    match error {
+        ResourceError::NotFoundById(id) => HttpResponse::NotFound().json(ApiResponse::<String> {
+            success: false,
+            data: None,
+            error: Some(ResponseError {
+                code: 404,
+                message: String::from("Requested resource not found"),
+            }),
+        }),
+        _ => HttpResponse::Gone().json(ApiResponse::<String> {
+            success: false,
+            data: None,
+            error: Some(ResponseError {
+                code: 404,
+                message: String::from("idk bruh"),
+            }),
+        }),
+    }
 }
 
 // // TODO: Update user
