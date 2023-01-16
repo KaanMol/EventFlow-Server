11669db6efe32bff5698eab404f49f7b8829c85d
Kaan <kaanmol@outlook.com>
2023-01-16 03:03:33 +0100

    Moved mongodb to handler in create source and cleaned read source

--- src/routes/source.rs
+++ src/routes/source.rs
@@ -1,8 +1,8 @@
 use crate::entity::user::CalendarEventSource;
 use crate::handlers::error::ResourceError;
 use crate::handlers::response::ApiResponse;
-use crate::routes::{parse_id, to_bson};
-use crate::{entity, AppState};
+use crate::routes::parse_id;
+use crate::{entity, handlers, AppState};
 use actix_web::web::{Data, Json, Path};
 use mongodb::results::UpdateResult;
 
@@ -15,15 +15,13 @@ pub struct CreateSourceBody {
     user_id: String,
 }
 
-// FIXME: Proper return type instead of UpdateResult
 #[actix_web::post("/sources")]
-pub async fn create(state: Data<AppState>, body: Json<CreateSourceBody>) -> Response<UpdateResult> {
+pub async fn create(
+    state: Data<AppState>,
+    body: Json<CreateSourceBody>,
+) -> Response<CalendarEventSource> {
     // TODO: Validate URL
-
     let id = parse_id(&body.user_id)?;
-    let filter = mongodb::bson::doc! {
-        "_id": id
-    };
 
     let new_source = entity::user::CalendarEventSource {
         name: body.name.clone(),
@@ -32,20 +30,9 @@ pub async fn create(state: Data<AppState>, body: Json<CreateSourceBody>) -> Resp
         modifiers: vec![],
     };
 
-    let update = mongodb::bson::doc! {
-        "$push": {
-            "sources": to_bson(new_source)
-        }
-    };
-
-    let result = state
-        .db
-        .collection::<entity::user::User>("users")
-        .update_one(filter, update, None)
-        .await
-        .map_err(|_| ResourceError::FailedDatabaseConnection)?;
-
-    Ok(ApiResponse::from_data(result))
+    Ok(ApiResponse::from_data(
+        handlers::source::create_source(id, new_source, state).await?,
+    ))
 }
 
 #[actix_web::get("/sources/{user_id}")]
@@ -53,22 +40,11 @@ pub async fn read(
     state: Data<AppState>,
     user_id: Path<String>,
 ) -> Response<Vec<CalendarEventSource>> {
-    // TODO: Validate URL
-
     let id = crate::routes::parse_id(&user_id)?;
 
-    let user = state
-        .db
-        .collection::<entity::user::User>("users")
-        .find_one(
-            mongodb::bson::doc! {
-                "_id": id
-            },
-            None,
-        )
+    let user = crate::handlers::user::get_user(id, state)
         .await
-        .map_err(|_| ResourceError::FailedDatabaseConnection)?
-        .ok_or_else(|| ResourceError::NotFoundById(id.to_string()))?;
+        .map_err(|_| ResourceError::NotFoundById(id.to_string()))?;
 
     Ok(ApiResponse::from_data(user.sources))
 }
