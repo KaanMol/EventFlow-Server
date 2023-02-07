29e23ffc18df1ee7ad2d1347720029f96f43276d
Kaan <kaanmol@outlook.com>
2023-02-07 18:24:54 +0100

    Added beginning of including authorization header openapi

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,4 +1,25 @@
 use crate::handlers::{error::ResourceError, response::ApiResponse};
 pub mod database;
+use utoipa::{
+    openapi::security::{ApiKey, ApiKeyValue, HttpAuthScheme, HttpBuilder, SecurityScheme},
+    Modify,
+};
 
 pub type Response<T> = std::result::Result<ApiResponse<T>, ResourceError>;
+
+pub struct SecurityAddon;
+
+impl Modify for SecurityAddon {
+    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+        let components = openapi.components.as_mut().unwrap();
+        components.add_security_scheme(
+            "api_key",
+            SecurityScheme::Http(
+                HttpBuilder::new()
+                    .scheme(HttpAuthScheme::Bearer)
+                    .bearer_format("JWT")
+                    .build(),
+            ),
+        )
+    }
+}
