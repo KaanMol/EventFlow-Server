ecfeb021e5f6689a5272dcf3271f695f5069b772
Kaan <kaanmol@outlook.com>
2023-02-14 22:42:43 +0100

    Added CreateEventDto and implemented EventDto from

--- src/app/events/dto.rs
+++ src/app/events/dto.rs
@@ -11,3 +11,27 @@ pub struct EventDto {
     pub all_day: bool,
     pub location: String,
 }
+
+impl From<crate::entity::event::EventEntity> for EventDto {
+    fn from(event: crate::entity::event::EventEntity) -> Self {
+        EventDto {
+            id: event.id.unwrap().to_string(),
+            title: event.title,
+            description: event.description,
+            start: event.start,
+            end: event.end,
+            all_day: event.all_day,
+            location: event.location,
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct CreateEventDto {
+    pub title: String,
+    pub description: String,
+    pub start: String,
+    pub end: String,
+    pub all_day: bool,
+    pub location: String,
+}
