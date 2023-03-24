3c8af030db1511d916392ac3ee9d115b37be40d3
Lucas <somfic@gmail.com>
2023-03-24 21:25:02 +0100

    Removed Modifiers and Filters

--- src/entity/user.rs
+++ src/entity/user.rs
@@ -23,36 +23,6 @@ impl fmt::Display for User {
 pub struct CalendarEventSource {
     pub name: String,
     pub url: String,
-    pub modifiers: Vec<CalendarEventSourceModifier>,
-    pub filters: Vec<CalendarEventSourceFilters>,
-}
-
-impl fmt::Display for CalendarEventSource {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", super::to_json(self))
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CalendarEventSourceModifier {
-    pub operation: String, // TODO: Convert to enum
-    pub field: String,     // TODO: Convert to enum
-    pub value: String,
-    pub new_value: String,
-}
-
-impl fmt::Display for CalendarEventSourceModifier {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", super::to_json(self))
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CalendarEventSourceFilters {
-    pub field: String,    // TODO: Convert to enum
-    pub operator: String, // TODO: Convert to enum
-    pub value: String,
-    pub calendar_id: String,
 }
 
 impl fmt::Display for CalendarEventSourceFilters {
