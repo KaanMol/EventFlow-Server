d8d5548a04044ae811f3ddf7446a381216f088db
Kaan <kaanmol@outlook.com>
2023-03-29 02:10:26 +0200

    test(tests): :white_check_mark: Added setup method for tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,1 +1,13 @@
-mod user;
+use crate::{app, common};
+
+pub mod data;
+mod events;
+mod users;
+
+pub async fn setup() -> app::State {
+    dotenv::dotenv().ok();
+
+    app::State {
+        db: common::database::connect().await,
+    }
+}
