2b830dee561face51e204776cec59bae7908afe9
Lucas <somfic@gmail.com>
2023-03-29 14:43:21 +0200

    Use environment variable for token.pem instead of file
    The .env variable now looks for a base64 (URL-safe-encoded) TOKEN_PEM variable that contains the public key.

--- src/app/middleware.rs
+++ src/app/middleware.rs
@@ -1,16 +1,37 @@
 use actix_web::{dev::ServiceRequest, HttpMessage};
 use actix_web_httpauth::extractors::bearer::BearerAuth;
+use base64::Engine;
 
 pub async fn auth_validator(
     req: ServiceRequest,
     credentials: BearerAuth,
 ) -> Result<ServiceRequest, (actix_web::Error, ServiceRequest)> {
+    // TODO: move this to documentation
+    // generate correct pem with:
+    // openssl rsa -pubout -in rsa-private.pem -out rsa-public.pem
+    let env_bytes_base64 = match std::env::var("TOKEN_PEM") {
+        Ok(val) => val.into_bytes(),
+        Err(_) => {
+            tracing::error!("Could not find TOKEN_PEM in environment variables");
+            return Err((
+                actix_web::error::ErrorInternalServerError("Authorization check failed"),
+                req,
+            ));
+        }
+    };
+
+    let base64_engine = base64::engine::GeneralPurpose::new(
+        &base64::alphabet::URL_SAFE,
+        base64::engine::general_purpose::NO_PAD,
+    );
+
+    let decoded_bytes = &base64_engine.decode(&env_bytes_base64).unwrap();
+
+    let key = &jsonwebtoken::DecodingKey::from_rsa_pem(decoded_bytes).unwrap();
+
     let result = jsonwebtoken::decode::<crate::app::Claims>(
         credentials.token(),
-        // TODO: move this to documentation
-        // generate correct pem with:
-        // openssl rsa -pubout -in rsa-private.pem -out rsa-public.pem
-        &jsonwebtoken::DecodingKey::from_rsa_pem(include_bytes!("../certs/token.pem")).unwrap(),
+        key,
         &jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::RS256),
     )
     .map_err(|e| actix_web::error::ErrorUnauthorized(e.to_string()));
