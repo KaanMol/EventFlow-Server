cb9f17162755f54125d3446db3ccb03597f0b0da
Lucas <somfic@gmail.com>
2023-03-31 00:43:51 +0200

    Refactored `create` and `delete` for sources
    Both the `create` and `delete` handlers now use the `user::update` handler in order to apply changes to the routes. This way the logic of creating and deleting routes in centralized in once place in the codebase.

--- src/app/sources/routes.rs
+++ src/app/sources/routes.rs
@@ -1,34 +1,46 @@
-use actix_web::{get, post, web::Json};
-
-use crate::{common, entity, handlers};
+use crate::{app::sources::dto::EventsSourceDto, entity, handlers};
+use actix_web::{delete, get, post, web::Json};
 
 use crate::{
-    app::{
-        sources::dto::{CreateEventSourceDto, EventsSourceDto},
-        AppState, UserClaims,
-    },
+    app::{AppState, UserClaims},
     handlers::response::ApiResponse,
 };
 
 #[post("")]
 pub async fn create(
+    body: Json<EventsSourceDto>,
     state: AppState,
-    body: Json<CreateEventSourceDto>,
     user_claims: UserClaims,
-) -> common::Response<EventsSourceDto> {
-    let source = entity::user::EventSource {
-        name: body.name.clone(),
-        url: body.url.clone(),
-    };
+) -> crate::common::Response<entity::user::User> {
+    let result = handlers::source::create_source(
+        user_claims.into_inner().cid,
+        body.into_inner().into(),
+        state,
+    )
+    .await?;
 
-    let created_source =
-        handlers::source::create_source(user_claims.into_inner().cid, source, state).await?;
+    Ok(ApiResponse::from_data(result))
+}
 
-    Ok(ApiResponse::from_data(created_source.into()))
+#[delete("")]
+pub async fn delete(
+    body: Json<EventsSourceDto>,
+    state: AppState,
+    user_claims: UserClaims,
+) -> crate::common::Response<entity::user::User> {
+    let result = handlers::source::delete_source(
+        user_claims.into_inner().cid,
+        body.into_inner().into(),
+        state,
+    )
+    .await?;
+
+    Ok(ApiResponse::from_data(result))
 }
 
 #[get("/sync")]
 pub async fn sync(state: AppState, user_claims: UserClaims) -> crate::common::Response<()> {
     let result = handlers::source::sync_sources(user_claims.into_inner().cid, state).await?;
+
     Ok(ApiResponse::from_data(result))
 }
--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -1,7 +1,3 @@
-// use crate::entities::{self};
-
-use crate::entity;
-
 use super::error::ResourceError;
 
 pub async fn get_user(
@@ -43,14 +39,50 @@ pub async fn update_user(
     user: crate::entity::user::User,
     state: actix_web::web::Data<crate::app::State>,
 ) -> Result<crate::entity::user::User, super::error::ResourceError> {
+    let mut updated_user = get_user(user.auth_id.clone(), state.clone()).await?;
+
+    let mut added_sources = Vec::new();
+
+    // Find all the sources that are new
+    for source in user.sources.clone() {
+        if updated_user.sources.contains(&source) == false {
+            added_sources.push(source);
+        }
+    }
+
+    // Remove all the sources that are no longer in the user's list
+    for source in updated_user.sources.clone() {
+        if user.sources.contains(&source) == false {
+            updated_user.sources.retain(|x| x != &source);
+        }
+    }
+
+    // Check if the new sources are valid, if they are, add them to the user's list
+    // TODO: These error messages should be more descriptive and describe *which* source is invalid.
+    for new_source in added_sources {
+        if new_source.name.len() == 0 {
+            // return Err(ResourceError::InvalidInput("name".to_string()));
+            continue;
+        }
+
+        let regex_source = r"^(https?|webcals)://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
+        let url_regex = regex::Regex::new(regex_source).unwrap();
+        if !url_regex.is_match(&new_source.url) {
+            // return Err(ResourceError::InvalidInput("url".to_string()));
+            continue;
+        }
+
+        updated_user.sources.push(new_source);
+    }
+
     let filter = mongodb::bson::doc! {
-        "auth_id": user.clone().auth_id
+        "auth_id": user.auth_id.clone()
     };
 
     state
         .db
         .collection::<crate::entity::user::User>("users")
-        .replace_one(filter, &user, None)
+        .replace_one(filter, &updated_user, None)
         .await
         .map_err(|_| ResourceError::FailedDatabaseConnection)?;
 
