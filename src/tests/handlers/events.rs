2c24b26239b69a045cd7983da7362cd0d1df2bc6
Lucas <somfic@gmail.com>
2023-03-30 17:27:02 +0200

    Added test for delete event handler

--- src/tests/handlers/events.rs
+++ src/tests/handlers/events.rs
@@ -40,3 +40,41 @@ pub async fn get_event() {
 
     assert_eq!(events.get(0).unwrap().title, "Example Event");
 }
+
+#[actix_rt::test]
+pub async fn delete_event() {
+    let state = tests::setup().await;
+    let user_id: String = "delete_event".to_string();
+
+    state
+        .db
+        .collection::<entity::event::EventEntity>("events")
+        .insert_one(tests::data::events(&user_id).get(0).unwrap(), None)
+        .await
+        .unwrap();
+
+    let events =
+        crate::handlers::events::get_all(user_id.clone(), actix_web::web::Data::new(state.clone()))
+            .await
+            .unwrap();
+
+    assert_eq!(events.len(), 1);
+
+    let event_id = events
+        .get(0)
+        .unwrap()
+        .id
+        .clone()
+        .expect("Event ID is missing");
+
+    crate::handlers::events::delete(event_id, actix_web::web::Data::new(state.clone()))
+        .await
+        .unwrap();
+
+    let events =
+        crate::handlers::events::get_all(user_id.clone(), actix_web::web::Data::new(state.clone()))
+            .await
+            .unwrap();
+
+    assert_eq!(events.len(), 0);
+}
