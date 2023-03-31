f161d76844dd1ddb3389aa96fed5febbe3396236
Lucas <somfic@gmail.com>
2023-03-31 15:43:08 +0200

    Added logging to tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -5,6 +5,8 @@ mod handlers;
 
 pub async fn setup() -> app::State {
     dotenv::dotenv().ok();
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::try_init().ok();
 
     app::State {
         db: common::database::connect_testdb().await,
