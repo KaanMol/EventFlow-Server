055f814106d66e1a618a1325690df748c6537eec
Lucas <somfic@gmail.com>
2023-04-01 22:39:50 +0200

    Added `test_get_not_existing_user` test

--- src/tests/integration/users.rs
+++ src/tests/integration/users.rs
@@ -38,4 +38,21 @@ async fn test_get_user_ok() {
 
     let body = resp.data.unwrap();
     assert_eq!(body.auth_id, auth_id);
+#[actix_web::test]
+async fn test_get_not_existing_user() {
+    let auth_id = "test_get_not_existing_user".to_string();
+    let state = tests::setup().await;
+    let app = tests::get_integration_app(state.clone(), auth_id.clone());
+
+    let service =
+        test::init_service(app.service(web::scope("/").service(crate::app::users::routes::read)))
+            .await;
+
+    let request = test::TestRequest::get().uri("/").to_request();
+    let response: handlers::response::ApiResponse<crate::app::users::dto::UserDto> =
+        test::call_and_read_body_json(&service, request).await;
+
+    assert!(response.data.is_none());
+    assert!(response.error.is_some());
+    assert_eq!(response.error.unwrap().code, 404);
 }
