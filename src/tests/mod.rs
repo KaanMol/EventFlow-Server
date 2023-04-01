b45d12adc5bd43f1466f36adeac5797916923475
Kaan <kaanmol@outlook.com>
2023-04-01 20:58:08 +0200

    test(tests): :art: Added more fields to Claims

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -6,7 +6,10 @@ use actix_web::{
 };
 use jsonwebtoken::TokenData;
 
-use crate::{app, common, entity};
+use crate::{
+    app::{self, Claims, UserClaims},
+    common, entity,
+};
 
 pub mod data;
 pub mod handlers;
@@ -39,8 +42,17 @@ pub fn get_integration_app(
     App::new()
         .app_data(actix_web::web::Data::new(state))
         .wrap_fn(move |req, srv| {
-            req.extensions_mut().insert(TestClaims {
+            req.extensions_mut().insert(Claims {
                 cid: user_id.clone(),
+                name: "John Doe".to_string(),
+                nickname: "John".to_string(),
+                preferred_username: "John".to_string(),
+                given_name: "John".to_string(),
+                auth_time: 0,
+                iat: 0,
+                exp: 0,
+                sub: "".to_string(),
+                groups: vec!["testGroup".to_string()],
             });
 
             srv.call(req)
